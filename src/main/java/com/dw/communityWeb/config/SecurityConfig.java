3fa891fb130911b9e63de76850e2dda4711e4c60
hahahanDW <gksehddn8@naver.com>
2025-02-10 16:33:47 +0900

    csrf 비활성화 추가

--- src/main/java/com/dw/communityWeb/config/SecurityConfig.java
+++ src/main/java/com/dw/communityWeb/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -27,19 +25,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll() // 모든 요청을 허용
-                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                )
-                .formLogin(login -> login
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .permitAll()
-                );
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login
+                .loginPage("/login")
+                .permitAll()
+        );
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/")
+                .permitAll()
+        );
+
+        //http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }
